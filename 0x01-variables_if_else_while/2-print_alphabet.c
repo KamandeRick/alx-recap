80ded263a84993681c2be2511b42909882ea5cbd
KamandeRick <rickmwangi237@gmail.com>
2024-02-06 21:20:11 -0800

    add program to print lowercasealpha to stdout'

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*Main -Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 97; ch < 123; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
