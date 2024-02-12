935f0149e856e659e21a2c09026f2ccc5e8485de
KamandeRick <rickmwangi237@gmail.com>
2024-02-11 22:32:22 -0800

    add program to print sum of multpiles of 3 an 5 betwen 0 and 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int main() {
+    int i;
+    int j = 0;
+    for(i = 0; i < 1024; i++)
+    {
+        if((i % 3 == 0) || (i % 5 == 0))
+        {
+            j = j + i;
+            
+        }
+    }
+     printf("%d\n", j);
+
+    return 0;
+}
