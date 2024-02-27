6744affe31097cf23da760dd2c2ae60b5b195150
RickMwangi <rickmwangi237@gmail.com>
2024-02-27 11:09:25 -0500

    add program that prints 0-99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range (0, 100):
+    if i <10:
+        print("0{}{}".format(i, (', ')), end = " ")
+    elif (i >= 10) and (i != 100):
+        print("{}{}".format(i, (', ')), end = " ")
+    elif (i == 100):
+        print("{}".format(i))
