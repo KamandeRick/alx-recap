c989e7c3d13eac1b9fdcee68fc72b48d88e21274
KamandeRick <rickmwangi237@gmail.com>
2024-03-03 09:39:03 -0800

    add program to return tuple of lenth and first character in sentence

--- python/0x03-python-data_structures/8-multiple_returns.py
+++ python/0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = ""
-    first = ""
-    if len(sentence) < 1:
-        length = "0"
-        first = "None"
-        print("Length: {:d} - First character: {}".format(length, first))
-    else:
-        length = len(sentence)
-        first = sentence[0]
-        print("Length: {:d} - First character: {}".format(length, first))
+   #''' length = ""
+   #first = ""
+   #if len(sentence) < 1:
+        #length = "0"
+        #first = "None"
+        #print("Length: {} - First character: {}".format(length, first))
+    #else:
+        #length = len(sentence)
+        #first = sentence[0]
+        #print("Length: {} - First character: {}".format(length, first))'''
+    return len(sentence), sentence[0] if sentence else None
