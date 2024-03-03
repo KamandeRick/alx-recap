f63875006e15a39ac305be1e324259bec35f18d3
KamandeRick <rickmwangi237@gmail.com>
2024-03-03 02:49:39 -0800

    add general expression comment

--- python/0x03-python-data_structures/5-no_c.py
+++ python/0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,8 @@
 
 def no_c(my_string):
     new_string = ''.join([x for x in my_string if x != "c" and x != "C"])
+
+    '''using a generator expression instead of a list comprehension  could be more memory efficient for longer strings since it doesn't create an intemediary list
+    new _string = ''.join(x for x in my_string if x.lower() != "c" 
+    the .lower() is also more concise'''
     return new_string
