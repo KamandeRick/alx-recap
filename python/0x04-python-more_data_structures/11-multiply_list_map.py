f24e78b25ccb889a33583d6442ae0e0914503ed3
KamandeRick <rickmwangi237@gmail.com>
2024-03-04 07:10:46 -0800

    add function using map and lambda to return new list of old list items multiplied by n

--- python/0x04-python-more_data_structures/11-multiply_list_map.py
+++ python/0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-   # new_list = [i * number for i in my_list]
-   #return new_list
-   new_list =list(map(lambda x: x * number, my_list))
-   return new_list
+   return list(map(lambda x: x * number, my_list))
+
+#my_list = [1,2,3,4,5]
+#new_list = multiply_list_map(my_list, 4)
+#print(new_list)
+#print(my_list)
