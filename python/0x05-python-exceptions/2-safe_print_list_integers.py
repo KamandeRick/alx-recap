351f63c15fd3d40bda47b05f0876c1d63dc9d677
KamandeRick <rickmwangi237@gmail.com>
2024-03-05 07:31:33 -0800

    add function to print int with exep

--- python/0x05-python-exceptions/2-safe_print_list_integers.py
+++ python/0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    while x > 0:
+    for i in range(0, x):
         try:
-            print("{:d}".format(int(my_list[count])), end='')
+            print("{:d}".format(my_list[i]), end = "")
+            count += 1
+        except (TypeError, ValueError):
+            #print("", end="")
+            pass
+    print("")
+    return count
+
